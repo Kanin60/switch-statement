46d0488be15a6d1db0915133bd577966e72b4d40
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-02-27 10:19:24 +0100

    Klasse opg

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+
+let navne = ;
+
+switch (navne){
+    case "bo":
+    case "nicklas":
+    case "morten":
+    case "simon":
+    case "mads":
+    case "mikkel":
+    case "alexder":
+    case "rasmus":
+    case "issac":
+    case "amina":
+    case "cæcilie":
+    case "katrine":
+    case "kamilla":
+    case "patrick":
+    case "emil":
+     console.log("Ja sørme!")
+    break;
+    default:
+        console.log("næ, sørme!") 
+    }
+
+
